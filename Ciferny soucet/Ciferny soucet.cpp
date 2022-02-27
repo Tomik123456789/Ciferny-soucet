803d3d31091f599a0db267561f7de1a6c8eb14e1
Uzivatel <Uzivatel@POTA>
2022-02-27 20:12:09 +0100

    Přidejte soubory projektu.

---
+++ Ciferny soucet/Ciferny soucet.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <stdio.h>
+
+int main()
+{
+    int cislo;
+    int x;
+    int soucet = 0;
+    printf("Ciferny soucet ");
+    scanf("%d", &cislo);
+    
+    while (cislo)
+    {
+        x = cislo % 10;
+        soucet = soucet + x;
+        cislo = cislo / 10;
+    }
+    printf("Ciferny soucet je %d", soucet);
+    return 0;
+}
